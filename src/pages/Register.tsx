542a99e49032eca7a802f081b9ec94d0c1f6b17a
khanhngo.pix@gmail.com <khanhngo.pix@gmail.com>
2023-10-17 23:32:11 +0700

    u

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -35,7 +35,7 @@ const Register = () => {
                 }
             })
             .catch(err => {
-                toast.error(err.response.data.message);
+                toast.error(err?.response.data.message);
                 setIsLoading(false);
             })
     };
